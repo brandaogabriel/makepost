19b0d092403ed1ea90cf13fa87d4ceda7914f5bb
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-02-03 19:21:18 -0300

    chore: project created

---
+++ src/main/java/com/gabriel/dev/makepost/MakepostApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.dev.makepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakepostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakepostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/dev/makepost/MakepostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.dev.makepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakepostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
