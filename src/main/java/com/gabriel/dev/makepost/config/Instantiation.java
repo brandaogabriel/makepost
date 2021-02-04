1aa183471d66444fd252b5c3a7dcd21a4df984c5
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-02-04 11:01:30 -0300

    chore: add seed configuration for User

---
+++ src/main/java/com/gabriel/dev/makepost/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.gabriel.dev.makepost.config;
+
+import com.gabriel.dev.makepost.entities.User;
+import com.gabriel.dev.makepost.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostConstruct
+	public void createSeed() {
+		this.userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		this.userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
